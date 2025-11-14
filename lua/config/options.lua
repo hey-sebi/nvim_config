52bcc0e910c6d3a69981a792a3e54c99a7f7af1f
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2025-11-14 11:11:00 +0100

    Update some packages

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- ---------------------------------------------
+--  Windows specific settings
+-- ---------------------------------------------
+if vim.fn.has("win32") == 1 then
+  -- Use PowerShell Core instead of cmd as shell
+  vim.opt.shell = "pwsh.exe"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
