17b12ff913272412530fdf21499e4d1dd5028dd3
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2026-01-03 17:51:23 +0100

    Disable cmake formatting

---
+++ lua/plugins/cmake.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cmake = {
+          on_attach = function(client, _)
+            -- disable formatting for cmake-language-server
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
+          end,
+        },
+      },
+    },
+  },
+
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = function(_, opts)
+  --     opts.formatters = opts.formatters or {}
+  --     opts.formatters_by_ft = opts.formatters_by_ft or {}
+  --
+  --     local is_win = vim.fn.has("win32") == 1
+  --
+  --     if is_win then
+  --       -- Windows: ensure .CMD shim is executable by routing through cmd.exe
+  --       -- This is relevant as cmake-format is shimmed
+  --       opts.formatters.cmake_format_cmd = {
+  --         command = "cmd.exe",
+  --         args = { "/c", "cmake-format", "-" },
+  --         stdin = true,
+  --       }
+  --       opts.formatters_by_ft.cmake = { "cmake_format_cmd" }
+  --     else
+  --       -- Linux/macOS: direct invocation is fine
+  --       opts.formatters_by_ft.cmake = { "cmake-format" }
+  --     end
+  --   end,
+  -- },
+}
