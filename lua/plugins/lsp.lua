1f523e7fe1e40ff262cedd5f8b05f94aacc1b318
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2026-03-21 14:03:27 +0100

    Add lsp.lua with lazydev.nvim setup
    This allows us to use proper nvim lua development with LS support

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,23 @@
+-- We use this settings to inject lua developer API symbols so that the
+-- LUA language server properly knows about them.
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        -- Load the neovim type definitions
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "lazy.nvim", words = { "LazyVim" } },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {},
+      },
+    },
+  },
+}
