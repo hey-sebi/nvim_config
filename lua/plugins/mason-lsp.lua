44dcb3bd56f8b47c1435ce769114cfd9d6f5e20b
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2026-04-12 17:15:14 +0200

    [fix] Use lazyvim features to setup python environment

--- lua/plugins/mason-lsp.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mason-org/mason-lspconfig.nvim",
-  cond = not vim.g.vscode,
-  opts = {
-    -- When an lsp server is configured but missing, install it automatically.
-    automatic_installation = true,
-  },
-}
