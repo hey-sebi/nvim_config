888dd35de4e2363c38f63ef457da1bda4d1b83d2
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2026-07-12 14:06:44 +0200

    Blink optimization: enter accepts if completion list is navigated, consider local symbols to overcome LSP issues

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -25,14 +25,21 @@ return {
         },
         ["<C-n>"] = { "select_next", "fallback" },
         ["<C-p>"] = { "select_prev", "fallback" },
+        -- If we manually scroll down the menu with C-n, hitting Enter will
+        -- accept it. Otherwise, it just inserts a newline.
+        ["<CR>"] = { "accept", "fallback" },
         ["<S-CR>"] = { "fallback" },
-        ["<CR>"] = { "fallback" },
       },
       completion = {
         menu = { border = "rounded" },
         documentation = { auto_show = true, window = { border = "rounded" } },
       },
       signature = { enabled = true },
+      -- Ensure buffer text and paths are highly prioritized to compensate for
+      -- legacy indexing gaps
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
     },
   },
 }
