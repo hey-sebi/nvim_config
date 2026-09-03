d4a529ffd3059817b0f7e75ade83676b8802293e
Sebastian Klemm <68734496+hey-sebi@users.noreply.github.com>
2026-09-03 11:20:33 +0200

    [tweak] Avoid clangd AST errors for newly created/inindexed headers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,6 +16,18 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      -- Suppress benign clangd AST errors for newly created / unindexed header files
+      local orig_inlay_hint = vim.lsp.handlers["textDocument/inlayHint"]
+      vim.lsp.handlers["textDocument/inlayHint"] = function(err, result, ctx, config)
+        if err and (err.code == -32602 or (err.message and err.message:find("trying to get AST"))) then
+          return
+        end
+        if orig_inlay_hint then
+          return orig_inlay_hint(err, result, ctx, config)
+        end
+      end
+    end,
     opts = {
       servers = {
         lua_ls = {},
@@ -28,6 +40,7 @@ return {
             "--completion-style=detailed",
             "--function-arg-placeholders=false",
             "--fallback-style=llvm",
+            "--log=error", -- Only log actual errors, avoiding RPC info dumps to stderr
             "-j=4", -- Use 4 helper threads for indexing on multi-core CPU
           },
         },
